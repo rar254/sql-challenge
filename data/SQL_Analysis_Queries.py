37ace7318c49fd408703be5d7b60060d134a324a
Rafael <stephany@Stephanys-Air.fios-router.home>
2020-11-23 15:19:52 -0500

    uploaded saved data queries from SQL

---
+++ data/SQL_Analysis_Queries.py
@@ -0,0 +1,28 @@
+--List Employee details
+CREATE VIEW Employee_Sheet AS
+SELECT employees.emp_no AS "Employee Number", first_name AS "First Name", last_name AS "Last Name", sex as "Sex", salaries.salary AS "Salary"
+FROM employees
+INNER JOIN salaries on
+salaries.emp_no = employees.emp_no;
+
+SELECT * FROM Employee_Sheet;
+
+--Employee Hired 1986
+CREATE VIEW "Employees Hired(1986)" AS
+SELECT first_name AS "First Name", last_name AS "Last Name", "hire-date" AS "Hire Date"
+FROM employees
+WHERE "hire-date" BETWEEN '1986-01-01' AND '1986-12-31';
+
+SELECT * FROM "Employees Hired(1986)";
+
+--Department Manager Details
+CREATE VIEW "Department Manager" AS
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name
+FROM departments
+INNER JOIN dept_manager on 
+departments.dept_no = dept_manager.dept_no
+INNER JOIN employees on
+dept_manager.emp_no = employees.emp_no;
+
+SELECT * FROM "Department Manager";
+
