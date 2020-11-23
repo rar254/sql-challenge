c528549d612498168f38c6d14e605d499596f5a4
Rafael <stephany@Stephanys-Air.fios-router.home>
2020-11-23 15:35:09 -0500

    uploading more SQL queries

--- data/SQL_Analysis_Queries.py
+++ data/SQL_Analysis_Queries.py
@@ -26,3 +26,14 @@ dept_manager.emp_no = employees.emp_no;
 
 SELECT * FROM "Department Manager";
 
+--Department Employee Details
+CREATE VIEW "Department Employees" AS
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no;
+
+SELECT * FROM "Department Employees";
+
